598e1a02d87de3a1cdbf69522f5af413eb62d0a6
jonathancaron <jonathan.caron04@gmail.com>
2018-02-01 13:46:18 +0100

    Function add

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ let express = require('express');
 let session = require('cookie-session');
 let bodyParser = require('body-parser');
 
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 let app = express();
 let PORT = 3000;
 
-app.use(session({secret: 'mysecretpass'}));
+app.use(session({secret: 'mysecretpass'}))
+.use(function(req,res,next){
+  if(typeof(req.session.todolist) == "undefined"){
+    req.session.todolist = [];
+  }
+  next();
+})
+.get('/todo', function(req, res){
+  res.render('todo.ejs', {todolist: req.session.todolist});
+})
+.post('/todo/ajouter/', urlencodedParser, function(req, res){
+  if(req.body.newtodo != ''){
+    req.session.todolist.push(req.body.newtodo);
+  }
+  res.redirect('/todo');
+});
 
 
 app.listen(PORT, function(){
